545b311d97f396420bd8d9624232adeee77abe9f
Marijnone <marijn.moviat@hva.nl>
2018-03-11 14:29:12 +0100

    Serving images

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@
 var express = require('express')
 var db = require('../db')
 var helpers = require('./helpers')
+var path = require('path')
 
 module.exports = express()
   .set('view engine', 'ejs')
   .set('views', 'view')
   .use(express.static('static'))
+  .use('/image', express.static('db/image'))
+  
+  
   // TODO: Serve the images in `db/image` on `/image`.
   .get('/', all)
   /* TODO: Other HTTP methods. */
