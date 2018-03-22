811789f369433da6c21c151a1d6624a49091026d
Marijnone <marijn.moviat@hva.nl>
2018-03-22 15:54:00 +0100

    The final edition workingggggggg🦆

--- server/index.js
+++ server/index.js
@@ -31,27 +31,26 @@ module.exports = express()
 
   // .put('/:id', set)
   // .patch('/:id', change)
-  .delete("/:id", remove)
+  .delete("/dier/:id", remove)
   .listen(1902);
 
 console.log(process.env.DB_NAME); //check if .env is working it iss
 console.log(process.env.DB_HOST);
 
 var connection = mysql.createConnection({
-  // debug: true, //method to enable sql to print out debug info which is best thing 
+  // debug: true, //method to enable sql to print out debug info which is best thing
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
-connection.connect(function (err) {
-  if (err) throw (err)
+connection.connect(function(err) {
+  if (err) throw err;
   else {
     console.log("Connected to MYSQL");
   }
 });
-
 
 function all(req, res, next) {
   connection.query("SELECT * FROM dier", done);
@@ -60,13 +59,15 @@ function all(req, res, next) {
     if (err) {
       next(err);
     } else {
-      res.render("list.ejs", Object.assign({}, helpers, {
-        data: data
-      }));
+      res.render(
+        "list.ejs",
+        Object.assign({}, helpers, {
+          data: data
+        })
+      );
     }
   }
 }
-
 
 //   /* Support both a request for JSON and a request for HTML  */
 //   res.format({
@@ -89,123 +90,100 @@ function dieren(req, res) {
 
   if (anid === NaN) {
     notFound(400, res);
-  } 
+  }
 } //closing dieren
 
+function remove(req, res) {
+  //Function to remove sweet animals
+  var id = req.params.id;
+  console.log(id);
 
-    // } else {
-    //   notFound(404, res);
-    // }
+  connection.query("DELETE FROM dier WHERE id = ?", id, done);
 
-    // // Function to bundle errors and then error.ejs sees what kind of error trough the first parameter
-    // function notFound(error, res) {
-    //   var errorObject = {
-    //     errors: [
-    //       {
-    //         id: error,
-    //         title: error
-    //       }
-    //     ]
-    //   };
-    //   res.status(error).render("error.ejs", errorObject);
-    // }
-
-    function remove(req, res) {
-      //Function to remove sweet animals
-      var id = req.params.id;
-
-      try {
-        db.remove(id);
-        res.status(204);
-      } catch (error) {
-        if (db.removed(id)) {
-          notFound(410, res);
-        } else {
-          notFound(404, res);
-        }
-      }
+  function done(err) {
+    if (err) {
+      next(err);
+    } else {
+      res.json({ status: "ok" });
     }
+  }
+}
 
-    function form(req, res) {
-      res.render("form.ejs");
+function form(req, res) {
+  res.render("form.ejs");
+}
+
+//function to show animals
+
+function showAnimal(req, res, next) {
+  var id = req.params.id;
+
+  connection.query("SELECT * FROM dier WHERE ID = ?", id, done); //function to shwo animal
+
+  function done(err, data) {
+    if (err) {
+      next(err);
+    } else if (data.length === 0) {
+      next();
+    } else {
+      res.render(
+        "detail.ejs",
+        Object.assign({}, helpers, {
+          data: data[0]
+        })
+      );
     }
+  }
+}
 
+function addToDB(req, res, next) {
+  //function to add input to sql
+  var input;
 
-    //function to show animals
+  connection.query(
+    "INSERT INTO dier SET ?",
+    (input = {
+      name: req.body.name,
+      type: req.body.type,
+      place: req.body.place,
+      description: req.body.description,
+      sex: req.body.sex,
+      age: parseInt(req.body.age, 10),
+      size: req.body.size,
+      length: req.body.length,
+      vaccinated: req.body.vaccinated == 1,
+      declawed: req.body.declawed == 1,
+      primaryColor: req.body.primaryColor,
+      secondaryColor: req.body.secondaryColor,
+      weight: parseInt(req.body.weight, 10),
+      intake: req.body.intake
+    }),
+    done
+  );
 
+  //thanks to Tim Ruiterkamp for explaining and helping
+  if (input.type === "dog" || input.type === "rabbit") {
+    console.log("dog or rabbit");
+    input.declawed = undefined;
+    console.log(input.declawed);
+  } else if (input.type === "cat" && input.type != 0) {
+    console.log("cat");
+    input.declawed = true;
+  } else {
+    input.declawed = false;
+  }
 
-    function showAnimal(req, res, next) {
-      var id = req.params.id;
+  if (input.secondaryColor === "" || input.secondaryColor === undefined) {
+    input.secondaryColor = undefined;
+  }
 
-      connection.query("SELECT * FROM dier WHERE ID = ?", id, done); //function to shwo animal
-
-      function done(err, data) {
-
-        if (err) {
-          next(err);
-        } else if (data.length === 0) {
-          next();
-        } else {
-          res.render("detail.ejs", Object.assign({}, helpers, {
-            data: data[0]
-          }));
-        }
-      }
+  function done(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      res.redirect("/dier/" + data.insertId);
+      console.log(err);
+      console.log(data.insertId);
     }
-
-    function addToDB(req, res, next) { //function to add input to sql
-      var input;
-
-
-      connection.query(
-        "INSERT INTO dier SET ?",
-        input = {
-          name: req.body.name,
-          type: req.body.type,
-          place: req.body.place,
-          description: req.body.description,
-          sex: req.body.sex,
-          age: parseInt(req.body.age, 10),
-          size: req.body.size,
-          length: req.body.length,
-          vaccinated: req.body.vaccinated == 1,
-          declawed: req.body.declawed == 1,
-          primaryColor: req.body.primaryColor,
-          secondaryColor: req.body.secondaryColor,
-          weight: parseInt(req.body.weight, 10),
-          intake: req.body.intake
-        }, done)
-
-
-
-      //thanks to Tim Ruiterkamp for explaining and helping
-      if (input.type === "dog" || input.type === "rabbit") {
-        console.log("dog or rabbit");
-        input.declawed = undefined;
-        console.log(input.declawed);
-      } else if (input.type === "cat" && input.type != 0) {
-        console.log("cat");
-        input.declawed = true;
-      } else {
-        input.declawed = false;
-      }
-
-      if (input.secondaryColor === "" || input.secondaryColor === undefined) {
-        input.secondaryColor = undefined;
-      }
-
-      function done(err, data) {
-        if (err) {
-          next(err);
-        } else {
-          res.redirect('/dier/' + data.insertId);
-          console.log(err);
-          console.log(data.insertId);
-
-
-
-        }
-      }
-    }
-  
-
+  }
+}
--- src/index.js
+++ src/index.js
@@ -14,3 +14,34 @@ while (++index < length) {
   time.textContent = moment(time.dateTime, 'YYYY-MM-DD').fromNow()
   time.title = time.dateTime
 }
+
+var remove = document.querySelector('.remove')
+
+if (remove) {
+  remove.addEventListener('click',onremove)
+}
+if (remove.length) {
+  remove.forEach(function(button){
+    button.addEventListener('click', onremove)
+  })
+}
+
+function onremove(ev) {
+  var node = ev.target
+  var id = node.dataset.id
+
+  fetch('/dier/' + id, {method: 'delete'})
+  .then(onresponse)
+  .then(onreload, onfail)
+
+  function onresponse(res){
+    return res.json()
+  }
+
+  function onreload() {
+    window.location = '/'
+  }
+  function onfail(){
+    throw new Error ('çould not delete')
+  }
+}
